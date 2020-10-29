61532ebd36aa567152e8110bcab3bc934a1c6262
Kerry Buckley <kerryjbuckley@gmail.com>
2020-10-29 10:41:52 +0000

    Removed boilerplate code

--- lib/iterative_rotation_cypher.ex
+++ lib/iterative_rotation_cypher.ex
@@ -1,18 +1,2 @@
 defmodule IterativeRotationCypher do
-  @moduledoc """
-  Documentation for `IterativeRotationCypher`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> IterativeRotationCypher.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/iterative_rotation_cypher_test.exs
+++ test/iterative_rotation_cypher_test.exs
@@ -1,8 +1,4 @@
 defmodule IterativeRotationCypherTest do
   use ExUnit.Case
   doctest IterativeRotationCypher
-
-  test "greets the world" do
-    assert IterativeRotationCypher.hello() == :world
-  end
 end
